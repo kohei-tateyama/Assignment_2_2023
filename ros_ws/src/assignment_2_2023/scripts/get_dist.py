f639c663b459ecc082ac9a8e41b05cb271b112f3
root <koheitateyama@keio.jp>
2024-01-19 19:10:48 +0000

    fully functioning

--- ros_ws/src/assignment_2_2023/scripts/get_dist.py
+++ ros_ws/src/assignment_2_2023/scripts/get_dist.py
@@ -2,14 +2,29 @@
 
 import rospy
 from assignment_2_2023.msg import Position
+from geometry_msgs.msg import Point
+import math
+
+goal_x=None
+goal_y=None
 
 
-def callback(data):
+def callback_robot(data):
     rospy.loginfo("X=%f Y=%f vel_x=%f vel_y=%f", data.x, data.y, data.vel_x, data.vel_z)
+    if goal_x is not None and goal_y is not None:
+        distance = math.sqrt((data.x - goal_x)**2 + (data.y - goal_y)**2)
+        print("Distance of robot to target is: ", distance)
+
+def callback_goal(data):
+    global goal_x, goal_y
+    goal_x = data.x
+    goal_y = data.y
+    print("New goal set:", goal_x, goal_y)
 
 def get_position():
     rospy.init_node('get_dist', anonymous=True)
-    rospy.Subscriber('/position', Position, callback)
+    rospy.Subscriber('/goal_topic', Point, callback_goal)
+    rospy.Subscriber('/position', Position, callback_robot)
     rospy.spin()
 
     # implement a server 
--- ros_ws/src/assignment_2_2023/scripts/last_target.py
+++ ros_ws/src/assignment_2_2023/scripts/last_target.py
@@ -1,7 +1,16 @@
+#! /usr/bin/env python
+
 import rospy
+from geometry_msgs.msg import Point
+
+def callback(data):
+    print("Next goal is:",data.x , data.y)
+    pass
 
 def get_target():
     rospy.init_node('get_target', anonymous=True)
+
+    rospy.Subscriber('/goal_topic', Point, callback)
     # returns the last target sent by the user
 
 
